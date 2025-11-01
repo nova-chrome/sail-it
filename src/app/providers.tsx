8c54bf61f6b168a7221b349f8f7f2d2c96080ce1
Victor Avila <victoravilamorales95@gmail.com>
2025-10-31 22:50:56 -0700

    feat: refactor RootLayout to integrate Sidebar and Breadcrumb components, enhancing layout structure and navigation

---
+++ src/app/providers.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "next-themes";
+import { PropsWithChildren } from "react";
+
+export function Providers({ children }: PropsWithChildren) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
