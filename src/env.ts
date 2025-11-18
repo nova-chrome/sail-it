54234a6f505823524a09b5f3521e038e1ef7b1f1
Victor Avila <victoravilamorales95@gmail.com>
2025-11-17 21:01:40 -0700

    feat(env): ✨ add TAVILY package

--- src/env.ts
+++ src/env.ts
@@ -6,6 +6,7 @@ export const env = createEnv({
     BLOB_READ_WRITE_TOKEN: z.string().min(1),
     DATABASE_URL: z.url(),
     OPEN_AI_API_KEY: z.string().min(1),
+    TAVILY_API_KEY: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_API_URL: z.string().min(1),
@@ -15,6 +16,7 @@ export const env = createEnv({
     BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
     DATABASE_URL: process.env.DATABASE_URL,
     OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+    TAVILY_API_KEY: process.env.TAVILY_API_KEY,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
